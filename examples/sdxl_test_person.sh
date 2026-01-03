09a67f6d5eadc7ec6c183a48418ac2cb8679adf1
Prima24 <primajuliano17@gmail.com>
2026-01-04 01:30:58 +0700

    prsn

--- examples/sdxl_test_person.sh
+++ examples/sdxl_test_person.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TASK_ID="5f0c9bca-9669-41e6-82b0-b772ea64cfc1"
-MODEL="John6666/nova-anime-xl-pony-v5-sdxl"
-DATASET_ZIP="https://gradients.s3.eu-north-1.amazonaws.com/5505def56da89675_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20251221%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20251221T015129Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=b006855ed6da9dd449362d10c5b4346e4e0a5ccec6d66254ec01e066a96c9dd2"
+TASK_ID="198cdb25-4e39-4650-bba3-fa2dc3259bfd"
+MODEL="GHArt/Lah_Mysterious_SDXL_V4.0_xl_fp16"
+DATASET_ZIP="https://s3.eu-central-003.backblazeb2.com/gradients-validator/e0267ff723faf52e_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=00362e8d6b742200000000002%2F20260101%2Feu-central-003%2Fs3%2Faws4_request&X-Amz-Date=20260101T224432Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=27dcb3bfc9b3958a0cc901828abea472e526d24ae37f3d0d802a9a441a51b6c9"
 MODEL_TYPE="sdxl"
-EXPECTED_REPO_NAME="test_person-1"
+EXPECTED_REPO_NAME="198cdb25-4e39-4650-bba3-fa2dc3259bfd-test_person-1-gg"
 
 HUGGINGFACE_TOKEN=""
 HUGGINGFACE_USERNAME=""
@@ -18,7 +18,16 @@ mkdir -p "$OUTPUTS_DIR"
 chmod 700 "$OUTPUTS_DIR"
 
 echo "Downloading model and dataset..."
-docker run --rm   --volume "$CHECKPOINTS_DIR:/cache:rw"   --name downloader-image   trainer-downloader   --task-id "$TASK_ID"   --model "$MODEL"   --dataset "$DATASET_ZIP"   --task-type "ImageTask"
+docker run --rm \
+  --volume "$CHECKPOINTS_DIR:/cache:rw" \
+  --volume "$(pwd)/../scripts/core:/app/core:rw" \
+  --volume "$(pwd)/../trainer:/app/trainer:rw" \
+  --name downloader-image \
+  trainer-downloader \
+  --task-id "$TASK_ID" \
+  --model "$MODEL" \
+  --dataset "$DATASET_ZIP" \
+  --task-type "ImageTask"
 
 echo "Starting image training..."
 docker run --rm --gpus all   --security-opt=no-new-privileges   --cap-drop=ALL   --memory=32g   --cpus=8   --network none   --env TRANSFORMERS_CACHE=/cache/hf_cache   --volume "$CHECKPOINTS_DIR:/cache:rw"   --volume "$OUTPUTS_DIR:/app/checkpoints/:rw"   --name image-trainer-example   standalone-image-trainer   --task-id "$TASK_ID"   --model "$MODEL"   --dataset-zip "$DATASET_ZIP"   --model-type "$MODEL_TYPE"   --expected-repo-name "$EXPECTED_REPO_NAME"   --hours-to-complete 1
